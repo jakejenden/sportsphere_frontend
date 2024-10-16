4df5c28cf4b24edc8fc5f3b69831c2765886013f
TerrafirmaJake <95360676+TerrafirmaJake@users.noreply.github.com>
2024-10-16 14:38:04 +0100

    Add elevation data and graph for event GPXs

--- src/MapboxMap.tsx
+++ src/MapboxMap.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './MapboxMap.css'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiamFrZWplbmRlbiIsImEiOiJjbTFpMm5mOHcwaWo0MmpzaXBkZ2s2eWMzIn0.GV26kFhLYpyeTc_xNh6Stw';
 
@@ -14,11 +15,23 @@ interface GPXData {
   GPX: string; // GPX data in string format (can be base64 encoded)
 }
 
+interface ElevationPoint {
+  elevation: number;
+  distance: number;
+}
+
 interface LegendProps {
   gpxDataList: GPXData[];
   routeColors: string[];
   toggleRouteVisibility: (index: number) => void;
   visibleRoutes: boolean[]; // Tracks the visibility of each route
+}
+
+interface ElevationGraphProps {
+  elevationPoints: { elevation: number, distance: number }[];
+  graphTitle: string;
+  routeColor: string;
+  elevationGain: number;
 }
 
 function formatFileName(fileName: string): string {
@@ -30,6 +43,24 @@ function formatFileName(fileName: string): string {
 
   return formattedName;
 }
+
+const ElevationGraph: React.FC<ElevationGraphProps> = ({ elevationPoints, graphTitle, routeColor, elevationGain }) => {
+  return (
+    <div>
+      <h3>{ formatFileName(graphTitle) }</h3>
+      <h6>Elevation Gain: { elevationGain } m</h6>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={elevationPoints}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="distance" label={{ value: 'Distance (km)', position: 'insideBottomRight', offset: 0 }} interval={100} />
+          <YAxis label={{ value: 'Elevation (m)', angle: -90, position: 'insideLeft' }} />
+          <Tooltip />
+          <Line type="monotone" dataKey="elevation" stroke={ routeColor } activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
 const Legend: React.FC<LegendProps> = ({ gpxDataList, routeColors, toggleRouteVisibility, visibleRoutes }) => {
   return (
@@ -69,6 +100,9 @@ const MapboxMap: React.FC<MapboxMapProps> = ({ gpxDataList }) => {
   const mapInstance = useRef<mapboxgl.Map | null>(null);
   const [routeColors, setRouteColors] = useState<string[]>([]);
   const [visibleRoutes, setVisibleRoutes] = useState<boolean[]>([]); 
+  const [elevationPoints, setElevationPoints] = useState<ElevationPoint[][]>([]);
+  const [elevationGain, setElevationGain] = useState<number[]>([]);
+  const [graphTitle, setGraphTitle] = useState<string[]>([]);
 
   useEffect(() => {
     if (!mapInstance.current && mapContainerRef.current) {
@@ -101,9 +135,70 @@ const MapboxMap: React.FC<MapboxMapProps> = ({ gpxDataList }) => {
           gpx.parse(decodedGPX);
           const geojson = gpx.toGeoJSON();
 
+          let totalDistance = 0;
+
           if (geojson && geojson.features && geojson.features.length > 0) {
             const gpxRoute = 'gpxRoute' + String(index);
             const gpxRouteLine = 'gpxRouteLine' + String(index);
+
+            const containsSwim = GPXData.Key.toLowerCase().includes("swim");
+
+            if (!containsSwim) {
+              const elevationDataCalc = geojson.features
+                                            .flatMap((feature: GeoJSON.Feature) => {
+                                              const { geometry } = feature;
+                                              
+                                              if (geometry.type === 'LineString') {
+                                                const coordinates = geometry.coordinates as GeoJSON.Position[];
+                                        
+                                                return coordinates.map((coord: GeoJSON.Position, index: number) => {
+                                                  const elevation = coord[2]; // Elevation is the third element in the coordinates array
+                                                  
+                                                  // Calculate distance from the previous point
+                                                  let distance = 0;
+                                                  if (index > 0) {
+                                                    const prevCoord = coordinates[index - 1];
+                                                    const R = 6371000; // Radius of Earth in meters
+                                                    const φ1 = (prevCoord[1] * Math.PI) / 180;
+                                                    const φ2 = (coord[1] * Math.PI) / 180;
+                                                    const Δφ = ((coord[1] - prevCoord[1]) * Math.PI) / 180;
+                                                    const Δλ = ((coord[0] - prevCoord[0]) * Math.PI) / 180;
+                                        
+                                                    const a =
+                                                      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                                                      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+                                                    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                                        
+                                                    distance = R * c; // Distance between two points in meters
+                                                    totalDistance += distance;
+                                                  }
+                                        
+                                                  return {
+                                                    graphTitle: GPXData.Key,
+                                                    elevation: elevation || 0, // Default to 0 if elevation is missing
+                                                    distance: parseFloat((totalDistance / 1000).toFixed(1)), // Cumulative distance
+                                                  };
+                                                });
+                                              }
+                                              
+                                              // Return an empty array if geometry does not have coordinates
+                                              return [];
+                                            })
+                                            .filter((elevation: number | undefined) => elevation !== undefined);
+              
+              setElevationPoints((prevData) => [...prevData, elevationDataCalc]);
+              setGraphTitle((prevData) => [...prevData, GPXData.Key]);
+              
+              let totalElevationGain = 0
+              for (let i = 1; i < elevationDataCalc.length; i++) {
+                const gain = elevationDataCalc[i].elevation - elevationDataCalc[i - 1].elevation;
+                if (gain > 0) {
+                    totalElevationGain += gain;
+                }
+              }
+  
+              setElevationGain((prevData) => [...prevData, parseFloat(totalElevationGain.toFixed(1))]);
+            }
 
             if (!map.getSource(gpxRoute)) { // Check if the source already exists
               map.addSource(gpxRoute, {
@@ -208,6 +303,16 @@ const MapboxMap: React.FC<MapboxMapProps> = ({ gpxDataList }) => {
       toggleRouteVisibility={toggleRouteVisibility}
       visibleRoutes={visibleRoutes}
     />
+    {elevationPoints.map((points, index) => (
+      <div>
+        <ElevationGraph
+          elevationPoints={points}
+          graphTitle={ graphTitle[index] }
+          routeColor={ routeColors[index] }
+          elevationGain={ elevationGain[index] }
+        />
+      </div>
+    ))}
   </div>
 };
 
